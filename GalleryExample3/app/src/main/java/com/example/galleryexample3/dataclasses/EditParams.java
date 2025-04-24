9a40448ed2cb33022c341f2bf357734f010b0ae7
KanCh <hagiabao1958@gmail.com>
2025-04-24 20:47:40 +0700

    completed edit activity
    - added crop ratio
    - added blur/sharpen

--- GalleryExample3/app/src/main/java/com/example/galleryexample3/dataclasses/EditParams.java
+++ GalleryExample3/app/src/main/java/com/example/galleryexample3/dataclasses/EditParams.java
@@ -5,15 +5,17 @@ public class EditParams {
     public float contrast;
     public float saturation;
     public String filter;
+    public int radius;
 
-    public EditParams(float brightness, float contrast, float saturation, String filter) {
+    public EditParams(float brightness, float contrast, float saturation, String filter, int radius) {
         this.brightness = brightness;
         this.contrast = contrast;
         this.saturation = saturation;
         this.filter = filter;
+        this.radius = radius;
     }
 
     public EditParams copy() {
-        return new EditParams(this.brightness, this.contrast, this.saturation, this.filter);
+        return new EditParams(this.brightness, this.contrast, this.saturation, this.filter, this.radius);
     }
 }
